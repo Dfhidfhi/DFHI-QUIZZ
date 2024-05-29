89e7ad585f171f3c4e913b1e0402636d29c43f71
DFHI-QUIZZ <ddfhiisfates@gmail.com>
2024-05-29 20:06:17 +0200

    UpdateHomePage

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -6,9 +6,8 @@
     <div class="text-about-us">
       <h1>Über uns</h1>
       <p>
-        Ich bin Aboubacar Camara, Student am DFHI-ISFATES. Im Rahmen meiner
-        Bachelorarbeit habe ich mich entschlossen, eine DFHI-Quiz-App zu
-        entwickeln, mit der man seine verschiedenen Vorlesungen von 6. Semester
+        Ich bin Aboubacar Camara, Student am DFHI-ISFATES. Es handelt sich um eine DFHI-Quiz-App zu
+        entwickeln, mit der man seine verschiedenen Vorlesungen 
         spielerisch wiederholen kann. Wer möchte, kann diese Gelegenheit nutzen,
         um seine Prüfungen zu bestehen.
       </p>
